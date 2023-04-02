ba94b64e2a072e0f56264bd73195ae604820b664
AnikeshDey <anikeshdey2000@gmail.com>
2023-04-02 10:40:06 +0530

    hardhat lottery app

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -5,9 +5,9 @@ const {ethers} = require("hardhat");
 const networkConfig = {
     5: {
         name: "goerli",
-        vrfCoordinator: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+        vrfCoordinator: "0xXXXXXXXXX",
         entranceFee: ethers.utils.parseEther("0.01"),
-        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+        gasLane: "0xXXXXXXXXXXXXXXxx",
         subscriptionId: "6926",
         callbackGasLimit: "50000", //500,00
         interval: "30"
@@ -16,7 +16,7 @@ const networkConfig = {
         name:"hardhat",
         subscriptionId: "588",
         entranceFee: ethers.utils.parseEther("0.01"),
-        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+        gasLane: "0xXXXXXXXXX",
         callbackGasLimit: "50000", //500,00
         interval: "30"
     }
